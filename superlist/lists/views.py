8c6f0175025c8f373ec0072c8ea5fac221742778
Aleksandr Zubakin <eldoson@yandex.ru>
2025-01-16 17:02:45 +0500

    refactor view home page

--- superlist/lists/views.py
+++ superlist/lists/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.shortcuts import render
 
 
 def home_page(request):
     """Домашняя страница"""
-    return HttpResponse('<html><title>To-Do lists</title></html>')
+    return render(request, 'home.html')
